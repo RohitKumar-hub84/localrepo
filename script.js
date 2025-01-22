c3b4bf2052fb1249095254cbe2a019cbe94ca7fd
RohitKumar-hub84 <rohitkumarbbm84093@gmail.com>
2025-01-22 14:57:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-
+function addTask() {
+    const newTask = document.getElementById('li')
+    const taskList = document.getElementById('taskList')
+    taskList.appendChild(newTask)
+    newTask.textContent = document.getElementById('inputTask').value
+    document.getElementById('inputTask').value=""
+    deleteTask(newTask)
+}
+function deleteTask(newTask)
+{
+    const deleteBtn= document.createElement('button')
+    deleteBtn.textContent="Delete"
+    newTask.appendchild(deleteBtn)
+    deleteBtn.onclick=function(){
+        newTask.remove()
+    }
+}
